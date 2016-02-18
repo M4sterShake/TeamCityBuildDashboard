1b456e2ff13886c64c46f801d5b406bacda4c1d8
M4stershake <maxborkowski@hotmail.com>
2016-02-18 23:55:03 +0000

    Moving ServerInfo section to handlebars template and its own class

--- TeamCityDashboard/app/scripts/serverInfo.js
+++ TeamCityDashboard/app/scripts/serverInfo.js
@@ -1,10 +1,57 @@
 ﻿ServerInfo = function () {
   this.Server = null;
+  this.teamCityApi = null;
 
+  var that = this;
+  var serverInfoRootId = "#server-settings";
+
+  this.Show = function(containerId) {
+    if ($(serverInfoRootId).length) {
+      $(serverInfoRootId).remove();
+    }
+
+    var template = Handlebars.templates['ServerInfoTemplate'](that.Server);
+    $(containerId).append(template);
+    $(serverInfoRootId).fadeIn();
+    displayServerProjects("#projects-and-builds-list-container");
+  }
+
+  function displayServerProjects(targetElement) {
+    that.teamCityApi.GetProjectsForServer(that.Server, function (err, projects) {
+      if (err) {
+        displayError("failed to send get request to " + that.server.url);
+        return;
+      }
+      $targetElement = $(targetElement);
+      $targetElement.html("");
+      displayProjectTree(projects, null, $targetElement);
+    });
+  }
+
+  function displayProjectTree(projects, rootProject, targetElement) {
+    if (projects != null) {
+      if (rootProject == null) { rootProject = "_Root" }
+      for (var i = 0; i < projects.length; i++) {
+        if (projects[i].id !== "_Root" && projects[i].parentProjectId === rootProject) {
+          if (targetElement.children("ul").length === 0) {
+            targetElement.append("<ul class='project-list'></ul>");
+          }
+          var targetList = targetElement.children("ul.project-list");
+          var projectListItem = $("<li class='closed'><i class='fa fa-cubes'></i>" + projects[i].name + "</li>");
+          projectListItem.on("click", function (e) {
+            $(this).toggleClass("closed");
+          });
+          targetList.append(projectListItem);
+          displayProjectTree(projects, projects[i].id, targetList);
+        }
+      }
+    }
+  }
 }
 
-ServerInfo.init = function (server) {
+ServerInfo.init = function (server, teamCityApi) {
   var serverInfo = new ServerInfo();
   serverInfo.Server = server;
-  return new ServerInfo();
+  serverInfo.teamCityApi = teamCityApi;
+  return serverInfo;
 };
--- TeamCityDashboard/app/scripts/settings.js
+++ TeamCityDashboard/app/scripts/settings.js
@@ -1,6 +1,7 @@
 ﻿(function ($) {
   var state,
-    teamCityApi;
+    teamCityApi,
+    serverInfoSection;
 
   init();
 
@@ -46,7 +47,6 @@
   function serverListItemEventHandler(e) {
     e.preventDefault();
     displayServer($(this).text());
-    displayServerProjects();
   }
 
   function displayServer(serverName) {
@@ -56,45 +56,9 @@
         return;
       }
 
-      $("#tc-display-name-header-text").text(serverInfo.displayName);
-      $("#update-server-form .tc-display-name").val(serverInfo.displayName);
-      $("#update-server-form .tc-url").val(serverInfo.url);
-      $("#update-server-form .tc-user").val(serverInfo.username);
-      $("#update-server-form .tc-pass").val(serverInfo.password);
-      $("#server-settings").fadeIn();
-      displayServerProjects(serverInfo, $("#projects-and-builds-list-container"));
+      serverInfoSection = ServerInfo.init(serverInfo, teamCityApi);
+      serverInfoSection.Show("#container");
     });
-  }
-
-  function displayServerProjects(server, targetElement) {
-    teamCityApi.GetProjectsForServer(server, function(err, projects) {
-      if (err) {
-        displayError("failed to send get request to " + server.url);
-        return;
-      }
-      targetElement.html("");
-      displayProjectTree(projects, null, targetElement);
-    });
-  }
-
-  function displayProjectTree(projects, rootProject, targetElement) {
-    if (projects != null ) {
-      if (rootProject == null) { rootProject = "_Root" }
-      for (var i = 0; i < projects.length; i++) {
-        if (projects[i].id !== "_Root" && projects[i].parentProjectId === rootProject) {
-          if (targetElement.children("ul").length === 0) {
-            targetElement.append("<ul class='project-list'></ul>");
-          }
-          var targetList = targetElement.children("ul.project-list");
-          var projectListItem = $("<li class='closed'><i class='fa fa-cubes'></i>" + projects[i].name + "</li>");
-          projectListItem.on("click", function(e) {
-            $(this).toggleClass("closed");
-          });
-          targetList.append(projectListItem);
-          displayProjectTree(projects, projects[i].id, targetList);
-        }
-      }
-    }
   }
 
   function displayError(message) {
@@ -110,19 +74,11 @@
       }
       
       toggleNewServerForm();
-      clearNewServerForm();
       state.GetServers(function(servers) {
         displayServers(servers);
         displayServer(serverInfo.displayName);
       });
     });
-  }
-
-  function clearNewServerForm() {
-    $("#add-server-form .tc-display-name").val("");
-    $("#add-server-form .tc-url").val("");
-    $("#add-server-form .tc-user").val("");
-    $("#add-server-form .tc-pass").val("");
   }
 
   function displayServers(servers) {
